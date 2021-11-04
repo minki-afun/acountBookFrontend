e3d985c8090032516508c10ebbb2465d62655ccf
minki-afun <minki@a-fun.co.kr>
2021-11-04 21:44:07 +0900

    초기페이지 설계 진행중

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { createAppContainer } from "react-navigation"
-import { createStackNavigator } from "react-navigation-stack"
+import React, { useCallback, useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
 
 import MainScreen from './screens/MainScreen'
 
-const navigator = createStackNavigator(
-  {
-    Main: MainScreen
-  },
-  {
-    initialRouteName: "Main",
-    defaultNavigationOptions: {
-      // headerleft: ,
-      title: "AccountBook",
-      // headerRight: ,
-    },
-  }
-);
+const Stack = createStackNavigator();
 
-export default createAppContainer(navigator)
+const App = () => {
+
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect( async () => {
+    setTimeout(() => {setIsLoading(false)}, 5000)
+  })
+  
+  if (isLoading) {
+    return (
+      <View>
+        <Text style={{flex:1, justifyContent:'center',alignItems:'center', fontSize:20}}>Loading</Text>
+      </View>
+    )
+  } else {
+    return (
+      <NavigationContainer>
+        <MainScreen />
+        {/* <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen 
+            name="Main" 
+            component={MainScreen}
+            options={{
+              title: "가계부"
+            }}
+          />
+        </Stack.Navigator> */}
+      </NavigationContainer>
+    )
+  }
+}
+
+export default App
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons';
 
 import ApolloClient from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloProvider } from '@apollo/react-hooks'
 
+import { ContentTab } from '../components/AppTabNavigator'
+import { UserTab } from '../components/AppTabNavigator'
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          return <Image source={require("../assets/icon1.png")} style={{width: 40, height: 40}}/>
+        },
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'black',
+      })}
+    >
+      <Tab.Screen 
+        name="가계부 기록" 
+        component={ContentTab} 
+      />
+      <Tab.Screen 
+        name="사용자" 
+        component={UserTab} 
+      />
+    </Tab.Navigator>
+  )
+}
 
 const client = new ApolloClient({
   link: createHttpLink({ uri: "http://localhost:4000"}),
@@ -13,11 +43,10 @@ const client = new ApolloClient({
 })
 
 const MainScreen = () => {
+
   return (
     <ApolloProvider client={client}>
-      <View>
-        <Text>HIdddassss</Text>
-      </View>
+      <Tabs />
     </ApolloProvider>
   )
 }
